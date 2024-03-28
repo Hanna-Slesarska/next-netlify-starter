723d0654a8e55086e46df846ef1d63b5f43dcaca
Hanna-Slesarska <slesarskaya.a@gmail.com>
2024-03-28 14:36:44 -0500

    u

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,26 @@
 import Head from 'next/head'
-import Header from '@components/Header'
+import { attributes, react as HomeContent } from '../content/home.md'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  let { title, cats } = attributes
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Head>
+        <article>
+          <h1>{title}</h1>
+          <HomeContent />
+          <ul>
+            {cats.map((cat, k) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
       <Footer />
     </div>
   )
