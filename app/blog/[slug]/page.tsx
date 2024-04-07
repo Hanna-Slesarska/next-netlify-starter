7cd44a40727b06652ab68da5c55148b533cf5295
Hanna-Slesarska <slesarskaya.a@gmail.com>
2024-04-07 09:37:13 -0500

    u

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -22,7 +22,7 @@ const PostPage = (props: any) => {
   const slug = props.params.slug;
   const post = getPostContent(slug);
   return (
-    <div>
+    <>
       <div className="my-12 text-center">
         <h1 className="text-2xl text-slate-600 ">{post.data.title}</h1>
         <p className="text-slate-400 mt-2">{post.data.date}</p>
@@ -31,7 +31,7 @@ const PostPage = (props: any) => {
       <article className="prose">
         <Markdown>{post.content}</Markdown>
       </article>
-    </div>
+    </>
   );
 };
 
